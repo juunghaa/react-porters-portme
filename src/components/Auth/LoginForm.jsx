d686ea6c69840dd920f7450aa774e7ccf911072a
juunghaa <jun8ha@naver.com>
2025-07-23 02:04:16 +0900

    로그인 화면_에러 디자인까지 구현 완료

--- src/components/Auth/LoginForm.jsx
+++ src/components/Auth/LoginForm.jsx
@@ -8,10 +8,36 @@ export default function LoginForm({onLoginSuccess}) {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState(''); //유효성 검사 추가하기
+    const [emailError, setEmailError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+    const [loginError, setLoginError] = useState('');
 
     const handleSubmit = (e)=>{
         e.preventDefault(); //새로고침 방지하고 현재 이메일비번 콘솔출력
+        // setLoginError("이메일 또는 비밀번호가 올바르지 않아요"); //로그인 실패 테스트용 코드
+
         console.log('로그인 시도: ', {email, password});
+
+        let valid = true;
+
+        // 에러 초기화
+        setEmailError('');
+        setPasswordError('');
+
+        if (!email) {
+            setEmailError('이메일을 입력해주세요.');
+            valid = false;
+        } else if (!email.includes('@')) {
+            setEmailError('올바른 메일 형식으로 입력해주세요.');
+            valid = false;
+        }
+
+        if (!password) {
+            setPasswordError('비밀번호를 입력해주세요.');
+            valid = false;
+        }
+
+        if (!valid) return;
 
         // 백엔드 연동 코드 추가
         signInWithEmailAndPassword(auth, email, password)
@@ -21,25 +47,41 @@ export default function LoginForm({onLoginSuccess}) {
             onLoginSuccess(); //<<--상태업데이트페이지전환
         })
         .catch((error) => {
-            setError('로그인 실패: ' + error.message);
-        });  
+            // setError('로그인 실패: ' + error.message);
+            console.log(error.code); // 코드 예: "auth/user-not-found", "auth/wrong-password"
+            if (
+                error.code === "auth/user-not-found" ||
+                error.code === "auth/wrong-password"
+              ) {
+                setLoginError("이메일 또는 비밀번호가 올바르지 않아요");
+              } else {
+                setLoginError("로그인 중 오류가 발생했습니다");
+              }
+        });   
     };
 
     return(
         <form onSubmit={handleSubmit} className="login-form">
+            {loginError && (
+            <div className="login-alert">
+                ⚠️ {loginError}
+            </div>
+            )}
+            
             <h2 className="login-title">로그인하고 포트폴리오를 만들어보세요</h2>
             <p className="login-subtitle">지금까지 쌓은 경험을,<br />나만의 방식으로 정리할 수 있어요</p>
 
             <div className="input-group">
                 <label htmlFor="email">이메일 주소</label>
                 <input
-                type="email"
+                type="text"
                 id="email"
-                // placeholder="example@domain.com"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                required
+                className={emailError ? 'input-error' : ''}
                 />
+                {emailError && <p className="error-text">{emailError}</p>}
+
             </div>
 
             <div className="input-group">
@@ -47,11 +89,12 @@ export default function LoginForm({onLoginSuccess}) {
                 <input
                 type="password"
                 id="password"
-                // placeholder="********"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-                required
+                className={passwordError ? 'input-error' : ''}
                 />
+                {passwordError && <p className="error-text">{passwordError}</p>}
+
             </div>
 
             {error && <p className="error-text">{error}</p>}
@@ -67,26 +110,5 @@ export default function LoginForm({onLoginSuccess}) {
   {/* <NaverLoginButton /> 여기에 추가하면 돼 */}
 </form>
 
-
-        /* 기존 코드 ->  */
-
-            /* 이메일과 비밀번호 입력 폼 */
-        //     <input type="email" placeholder="이메일" value={email}
-        //     onChange={(e)=> setEmail(e.target.value)}
-        //     required />
-        //     {/* placeholder는 사용자에게 보여주는 회색 안내 문구임 */}
-        //     {/* 인풋에 값이 입력되어야만 폼이 제출될 수 있도록 유효성 검사하는게 required */}
-        //     <input type="password" placeholder="비밀번호" value={password}
-        //     onChange={(e)=>setPassword(e.target.value)}
-        //     required />
-
-        //     {/* 에러 메시지 출력 */}
-        //     {error && <p className="error-text">{error}</p>}
-
-        //     <button type="submit">로그인</button>
-
-        //     <GoogleLoginButton onLoginSuccess={onLoginSuccess} />
-
-        // </form>
     );
 }
