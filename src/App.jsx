a991a94a2bb109ee8f2f2f1964af833be63b1dae
juunghaa <jun8ha@naver.com>
2025-08-02 01:10:37 +0900

    프론트 코드 충돌 해결

--- src/App.jsx
+++ src/App.jsx
@@ -12,63 +12,72 @@ import ActivityEditorPage from './pages/ActivityEditorPage.jsx';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 
-
 // 나중에 로그인 이후 로그인폼 감추고 메인만 보이도록 정리하거나.. 페이지 관리 필요 
-// 주석 처리한게 원래 정하 코드!!
-export default function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+// 정하 코드!!
 
-  const handleLoginSuccess=()=>{
-    setIsLoggedIn(true);
-  };
-  const handleLogout=()=>{
-    setIsLoggedIn(false);
-    console.log("로그아웃 되었습니다.");
-  };
+// export default function App() {
+//   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  return (
-    <main>
-      {isLoggedIn ? (
-        <>
-        <h2>환영합니다앙</h2>
-        <LogoutButton onLogout={handleLogout}/>
-        </>) : (
-        <LoginForm onLoginSuccess={handleLoginSuccess}/>
-        )
-      }
-    </main>
-  );
-}
+//   const handleLoginSuccess=()=>{
+//     setIsLoggedIn(true);
+//   };
+//   const handleLogout=()=>{
+//     setIsLoggedIn(false);
+//     console.log("로그아웃 되었습니다.");
+//   };
 
-// App.jsx (일단 로그인 띄고 메인페이지만 보아게 수정)
-// import Header from './components/Header/Header';
-// import Sidebar from './components/Sidebar/Sidebar';
-// import RightPanel from './components/RightPanel/RightPanel';
+//   return (
+//     <main>
+//       <MainPage />
+//       <ActivityEditorPage />
+//       {isLoggedIn ? (
+//         <>
+//         <h2>환영합니다앙</h2>
+//         <LogoutButton onLogout={handleLogout}/>
+//         <MainPage />
 
-
-// function App() {
-//     return (
-//         <div className="App">
-//             {/* 사이드바, 메인 영역  배치 */}
-//             <div style={{display: 'flex', minHeight: '100vh'}}>
-//                 {/* 왼쪽 사이드바 */}
-//                 <Sidebar/>
-
-//                 {/* 가운데 메인 영역 */}
-//                 <div style={{flex: 1, display: 'flex', flexDirection: 'column'}}>
-//                     <Header/>
-//                     <div style={{padding: '20px', backgroundColor: '#f9fafb', flex: 1}}>
-//                         <h1>Header와 Sidebar를 테스트 중이다</h1>
-//                         <p>바쁘다 바빠</p>
-//                         <p>왼쪽 사이드바를 만들었어용</p>
-//                     </div>
-//                 </div>
-
-//                 {/* 오른쪽 패널 */}
-//                 <RightPanel/>
-//             </div>
-//         </div>
-//     );
+//         </>) : (
+//         <>
+//         <LoginPage onLoginSuccess={handleLoginSuccess}/>
+//         <SignupPage onLoginSuccess={handleLoginSuccess} />
+//         </>
+//         )
+//       }
+//     </main>
+//   );
 // }
 
-// export default App;
+// App.jsx (일단 로그인 띄고 메인페이지만 보아게 수정)
+// 단비 코드 !!
+
+import Header from './components/Header/Header';
+import Sidebar from './components/Sidebar/Sidebar';
+import RightPanel from './components/RightPanel/RightPanel';
+
+
+function App() {
+    return (
+        <div className="App">
+            {/* 사이드바, 메인 영역  배치 */}
+            <div style={{display: 'flex', minHeight: '100vh'}}>
+                {/* 왼쪽 사이드바 */}
+                <Sidebar/>
+
+                {/* 가운데 메인 영역 */}
+                <div style={{flex: 1, display: 'flex', flexDirection: 'column'}}>
+                    <Header/>
+                    <div style={{padding: '20px', backgroundColor: '#f9fafb', flex: 1}}>
+                        <h1>Header와 Sidebar를 테스트 중이다</h1>
+                        <p>바쁘다 바빠</p>
+                        <p>왼쪽 사이드바를 만들었어용</p>
+                    </div>
+                </div>
+
+                {/* 오른쪽 패널 */}
+                <RightPanel/>
+            </div>
+        </div>
+    );
+}
+
+export default App;
